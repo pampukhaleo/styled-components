375ba6770b29679b4b35fb10815c5e741308acbe
pampukhaleo <lgpampukha@gmail.com>
2023-07-15 17:12:33 +0300

    styled.tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box>
+        <StyledBtn>Button</StyledBtn>
+        <StyledBtn as='a' href='#'>Link</StyledBtn>
+        <SuperBtn>SuperBtn</SuperBtn>
+        <StyledLink>Styled Link</StyledLink>
+      </Box>
     </div>
   );
 }
 
 export default App;
+
+const StyledBtn = styled.button`
+  border: none;
+  background-color: #de5d5d;
+  padding: 10px 20px;
+  color: snow;
+  font-size: 2rem;
+  font-weight: bold;
+`
+
+const StyledLink = styled.a`
+  color: #254be5;
+  font-size: 2rem;
+  font-weight: bold;
+`
+
+const SuperBtn = styled(StyledBtn)`
+  border-radius: 5px;
+  background-color: aqua;
+  color: darkred;
+`
+const Box = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
